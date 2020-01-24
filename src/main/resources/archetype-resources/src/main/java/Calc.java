faa5e90db0688f44929ce9bcf71f18bb4a7cbee7
ledoyen <ledoyen.loic@gmail.com>
2020-01-24 11:10:07 +0100

    Add 4A 2020 Java archetype

---
+++ src/main/resources/archetype-resources/src/main/java/Calc.java
@@ -0,0 +1,12 @@
+package ${package};
+
+public class Calc {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+}
